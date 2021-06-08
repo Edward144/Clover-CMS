919b5cc10ac3c90b4e158b939d57cd4ee23d4a2f
Ed Hall <edward.hall.144@gmail.com>
2021-06-08 23:23:39 +0100

    Set up sidebar

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,9 @@
+<ul class="nav flex-column">
+	<a id="toggleSidebar" class="floatingToggle btn-dark"><span class="fa fa-bars"></span></a>
+	<li class="nav-item">
+		<a id="toggleSidebar" class="nav-link btn-dark">
+			<span>Toggle Menu</span>
+			<span class="fa fa-bars"></span>
+		</a>
+	</li>
+</ul>
