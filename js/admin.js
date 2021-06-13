bd02a6da402557a4bf8878ddcc0ae6c98c17fb43
Ed Hall <edward.hall.144@gmail.com>
2021-06-13 15:43:00 +0100

    Manage Users functionality

--- js/admin.js
+++ js/admin.js
@@ -34,3 +34,53 @@ function responsive_filemanager_callback(field_id) {
 $(document).ready(function() {
 	sidebarcollapse();
 });
+
+//Validate forms
+$("form").submit(function() {
+	var valid = true;
+	var passChar = 8;
+	
+	$(this).find(".invalid-feedback").remove();
+	$(this).find(".is-invalid").removeClass("is-invalid");
+	
+	//Validate Passwords
+	if($(this).find("input[name='password']") && $(this).find("input[name='passwordConf']")) {
+		var pass = $(this).find("input[name='password']");
+		var passConf = $(this).find("input[name='passwordConf']");
+		
+		if(pass.val().length || passConf.val().length) {
+			if(pass.val().length < passChar) {
+				pass.addClass("is-invalid");
+				$("<div class='invalid-feedback'>Password must be at least " + passChar + " characters</div>").insertAfter(pass);
+
+				valid = false;
+			}
+			else if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(pass.val())) {
+				pass.addClass("is-invalid");
+				$("<div class='invalid-feedback'>Password must contain at least one lowercase, one uppercase and one digit</div>").insertAfter(pass);
+
+				valid = false;
+			}
+			else if(pass.val() != passConf.val()) {
+				pass.addClass("is-invalid");
+				passConf.addClass("is-invalid");
+				$("<div class='invalid-feedback'>Passwords do not match</div>").insertAfter(passConf);
+
+				valid = false;
+			}
+		}
+	}
+	
+	if(valid == false) {
+		event.preventDefault();
+		return;
+	}
+});
+
+//Confirm inputs
+$("input[type='submit'][data-confirm]").click(function() {
+	if(!confirm($(this).attr("data-confirm"))) {
+		event.preventDefault();
+		return;
+	}
+});
