98eb21308febd265cc682430b55e768cfd59e388
Ed Hall <edward.hall.144@gmail.com>
2021-06-13 01:16:04 +0100

    Create manage-navigation.php

---
+++ admin/manage-navigation.php
@@ -0,0 +1,14 @@
+<?php 
+	$title = 'Navigation';
+	require_once(dirname(__FILE__) . '/includes/header.php'); 
+?>
+
+<div class="col-lg-3 bg-light py-3">
+	
+</div>
+
+<div class="col">
+
+</div>
+
+<?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
