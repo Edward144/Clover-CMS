0fe1f9e4d498d4b658a821a92133001f5581a357
Ed Hall <edward.hall.144@gmail.com>
2021-06-13 17:26:44 +0100

    Update frontend header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
+					</div>
+				</div>
 			</div>
 
 			<footer id="pageFooter">
-
+				footer
 			</footer>
 		</div>
 	</body>
---
+++ page.php
@@ -0,0 +1,5 @@
+<?php require_once(dirname(__FILE__) . '/includes/header.php'); ?>
+
+<?php var_dump($_GET); ?>
+
+<?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
