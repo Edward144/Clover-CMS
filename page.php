8768b5183dc36154b4bbbb3b9601339f15767d93
Ed Hall <edward.hall.144@gmail.com>
2021-06-22 22:40:43 +0100

    Initial page template structure

--- page.php
+++ page.php
@@ -1,5 +1,27 @@
-<?php require_once(dirname(__FILE__) . '/includes/header.php'); ?>
+<?php 
+    $content = $mysqli->prepare("SELECT * FROM `posts` WHERE id = ?");
+    $content->bind_param('i', $contentId);
+    $content->execute();
+    $contentResult = $content->get_result();
 
-<?php var_dump($_GET); ?>
+    if($contentResult->num_rows <= 0) {
+        http_response_code(404);
+        include_once($_SERVER['DOCUMENT_ROOT'] . ROOT_DIR . '404.php');
+        exit();
+    }
+
+    $content = $contentResult->fetch_assoc();
+
+    $title = (!empty($content['meta_title']) ? $content['meta_title'] : $content['name']);
+    $description = (!empty($content['meta_description']) ? $content['meta_description'] : $content['excerpt']);
+    $author = (!empty($content['meta_author']) ? $content['meta_author'] : $content['author']);
+    $keywords = (!empty($content['meta_keywords']) ? $content['meta_keywords'] : '');
+
+    require_once(dirname(__FILE__) . '/includes/header.php'); 
+?>
+
+<div class="uc content">
+    <?php parsecontent($content['content']); ?>
+</div>
 
 <?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
