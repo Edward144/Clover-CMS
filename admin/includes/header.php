33b42e2a022152b6671c5961f70dceee274022d7
Ed Hall <edward.hall.144@gmail.com>
2021-07-05 13:55:14 +0100

    Moved analytics function to correct header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -13,10 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="UTF-8">
 		
-		<?php 
-            adminmeta($title, $description, $keywords, $author); 
-            googleanalytics();
-        ?>
+		<?php adminmeta($title, $description, $keywords, $author); ?>
 		
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
