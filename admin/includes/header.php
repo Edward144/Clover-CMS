3629ba43acc50fa6ddde37efd3fc8661935d456f
Ed Hall <edward.hall.144@gmail.com>
2021-07-05 13:49:58 +0100

    Created google analytics insertion function

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -13,7 +13,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="UTF-8">
 		
-		<?php adminmeta($title, $description, $keywords, $author); ?>
+		<?php 
+            adminmeta($title, $description, $keywords, $author); 
+            googleanalytics();
+        ?>
 		
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
