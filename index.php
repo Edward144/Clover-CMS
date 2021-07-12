403f34d1d6d2887d631bc9aceb5185a98bca752e
Ed Hall <edward.hall.144@gmail.com>
2021-07-12 15:07:18 +0100

    Created 404 page

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
+    /* This file is used to redirect any requested url to the correct template file or if a template file does not exist then to page.php.
+     * If the homepage has been set and the page exists then that page will be redirected to / which will display that page's content.
+     * If the homepage doesn't exist or isn't set then / will display the content of the page with the lowest ID.
+     */
+     
 	require_once(dirname(__FILE__) . '/includes/database.php'); 
 	
     $state = (!empty($_SESSION['adminid']) ? 1 : 2);
@@ -41,17 +46,58 @@
 
 	$_GET['url'] = (empty($_GET['url']) ? '' : $_GET['url']);
     
+    //Remove first part of url if post type pages is included
+    $hasPostType = false;
+    $postTypes = $mysqli->query("SELECT name FROM `post_types`");
+    
+    if($postTypes->num_rows > 0) {
+        $ptA = [];
+        
+        while($pt = $postTypes->fetch_array()) {
+            array_push($ptA, $pt[0]);
+        }
+        
+        if(in_array(explode('/', $_GET['url'])[0], $ptA) && strpos($_GET['url'], explode('/', $_GET['url'])[0] . '/') === 0) {
+            $hasPostType = true;
+            
+            $url = explode('/', $_GET['url']);
+            array_shift($url);
+            $url = implode('/', $url);
+        }
+    }
+
+    if($hasPostType == false) {
+        $url = $_GET['url'];
+    }
+
 	$notFound = true;
 	$templatePath = dirname(__FILE__) . '/';
 	$template = 'page.php';
 		
-	$checkUrl = $mysqli->prepare("SELECT id, template FROM `posts` WHERE url = ? AND state >= ? LIMIT 1");
-	$checkUrl->bind_param('si', $_GET['url'], $state);
+	$checkUrl = $mysqli->prepare(
+        "SELECT posts.id, posts.template, posts.post_type_id, posts.url, post_types.name AS post_type FROM `posts` AS posts
+            LEFT OUTER JOIN `post_types` AS post_types ON post_types.id = posts.post_type_id
+        WHERE posts.url = ? AND posts.state >= ? LIMIT 1");
+	$checkUrl->bind_param('si', $url, $state);
 	$checkUrl->execute();
 	$checkUrlResult = $checkUrl->get_result();
 
 	if($checkUrlResult->num_rows > 0) {
 		$page = $checkUrlResult->fetch_assoc();
+        
+        if($page['post_type_id'] > 1 && strpos($_GET['url'], $page['post_type'] . '/') !== 0) {
+            //Redirect post types to include the prefix
+            http_response_code(301);
+            header('Location: ' . ROOT_DIR . $page['post_type'] . '/' . $page['url']);
+            exit();
+        }
+        elseif($page['post_type_id'] == 1 && strpos($_GET['url'], $page['post_type'] . '/') === 0) {
+            //Redirect pages to remove the prefix
+            http_response_code(301);
+            header('Location: ' . ROOT_DIR . $page['url']);
+            exit();
+        }
+        
 		$notFound = false;
 		$contentId = $page['id'];
         
@@ -65,6 +111,7 @@
 
 	if($notFound == true) {
 		http_response_code(404);
+        include_once(dirname(__FILE__) . '/404.php');
 	}
 	else {
 		require_once($templatePath . $template);
