b7c16beb7af246c6e9f79e22f12558d7fa012b1e
Ed Hall <edward.hall.144@gmail.com>
2021-08-08 17:06:14 +0100

    Created setup page

--- includes/database.php
+++ includes/database.php
@@ -2,17 +2,34 @@
 	
 	session_start();
 
-	require_once(dirname(__FILE__) . '/settings.php');
+	include_once(dirname(__FILE__) . '/settings.php');
+    $setupLoc = (!empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . '/' . explode('/', dirname(__DIR__))[count(explode('/', dirname(__DIR__))) - 1] . '/admin/setup';
+    
+    if($_SERVER['REQUEST_URI'] != explode($_SERVER['SERVER_NAME'], $setupLoc)[1]) {
+        if(!file_exists(dirname(__FILE__) . '/settings.php')) {
+            header('Location: ' . $setupLoc);
+            exit();
+        }
 
-	$mysqli = new mysqli($hostname, $username, $password, $database);
+        $mysqli = new mysqli($hostname, $username, $password, $database);
 
-	if($mysqli->connect_error) {
-		die('Connection Error: ' . $mysqli->connect_error);
-	}
+        if($mysqli->connect_error) {
+            die('Failed to connect to database');
+        }
 
-	unset($hostname);
-	unset($database);
-	unset($username);
-	unset($password);
+        //Check if setup is complete
+        $setupCheck = $mysqli->query("SELECT value FROM `settings` WHERE name = 'setup_complete' AND value = 1 LIMIT 1");
+
+        if($setupCheck->num_rows != 1) {
+            header('Location: ' . $setupLoc);
+            exit();
+        }
+
+        unset($hostname);
+        unset($database);
+        unset($username);
+        unset($password);
+        unset($setupLoc);
+    }
 
 ?>
