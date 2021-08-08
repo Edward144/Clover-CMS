05032cf20e196545a275657a2c8b275b93871a47
Ed Hall <edward.hall.144@gmail.com>
2021-08-08 21:27:23 +0100

    Created settings template file

---
+++ includes/settings.template.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Database Details
+	$hostname = '';
+    $database = '';
+    $username = '';
+	$password = '';
+
+	define('ROOT_DIR', '/');
+	define('BASE_DIR', (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . ROOT_DIR);
+
+?>
