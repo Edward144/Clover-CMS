a7a020f77a90d469b4d2ced26bceaa777e14c773
Ed Hall <edward.hall.144@gmail.com>
2021-08-21 13:39:30 +0100

    Clear post data afterwards

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -9,5 +9,14 @@
 
 		<script src="js/admin.min.js"></script>
 		<script src="js/tinyConfig.min.js"></script>
+
+        <?php if(!empty($_POST)) : ?>
+            <script>
+                //Remove post data to avoid resubmission dialog
+                if(window.history.replaceState) {
+                    window.history.replaceState(null, null, window.location.href);
+                }
+            </script>
+        <?php endif; ?>
 	</body>
 </html>
