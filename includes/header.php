2964439b8d72ebe4eae3660012bd83b97420a48d
Ed Hall <edward.hall.144@gmail.com>
2021-08-21 13:32:47 +0100

    Using boostrap bundle instead of separate popper.js

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,7 @@
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-		<script src="bootstrap-5.0.1/bootstrap.min.js"></script>
+		<script src="js/bootstrap.bundle.min.js"></script>
 		<script src="js/docRoot.min.js"></script>
 		<script src="js/tinymce/tinymce.min.js"></script>
 		<script src="js/jquery.fancybox.min.js"></script>
