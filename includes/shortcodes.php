f66c31ff697d31ca8f6a8cc27b3ac8af31a7fc11
Ed Hall <edward.hall.144@gmail.com>
2021-08-21 22:37:56 +0100

    Created contact form submission functionality

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -58,13 +58,18 @@
                                 '<div class="form-group mb-3" id="input' . $input['inputid'] . '">' .
                                     (!empty($input['label']) && !in_array($input['type'], ['button', 'submit', 'hidden', 'checkbox']) ? '<label' . ($input['required'] == true ? ' class="required"': '') . '>' . $input['label'] . '</label>' : '');
                             
+                            if(!in_array($input['type'], ['button', 'submit', 'hidden'])) {
+                                $output .=
+                                    '<input type="hidden" name="label' . $input['inputid'] . '" value="' . $input['label'] . '">';
+                            }
+                            
                             switch($input['type']) {
                                 case 'general': 
                                     $output .= $input['value'];
                                     break;
                                 case 'textarea': 
                                     $output .=
-                                        '<textarea class="form-control" name="' . $input['name'] . '" placeholder="' . $input['placeholder'] . '"' . ($input['required'] == true ? ' required' : '') . '>' . $input['value'] . '</textarea>';
+                                        '<textarea class="form-control" name="' . $input['inputid'] . '" placeholder="' . $input['placeholder'] . '"' . ($input['required'] == true ? ' required' : '') . '>' . $input['value'] . '</textarea>';
                                     break;
                                 case 'number': 
                                     $output .=
@@ -147,6 +152,55 @@
                             </div>';
                         
                         $gi++;
+                    }
+                    
+                    //Add Recaptcha validation if contact form
+                    if(strpos($structure['action'], 'actions/formsubmit.php') !== false) {
+                        $captcha = $mysqli->query("SELECT name, value FROM `settings` WHERE name = 'recaptcha_sitekey' OR name = 'recaptcha_secretkey'");
+                        
+                        if($captcha->num_rows > 0) {
+                            $cptch = [];
+                            
+                            while($row = $captcha->fetch_assoc()) {
+                                $cptch[$row['name']] = $row['value'];
+                            }
+                            
+                            $sitekey = $cptch['recaptcha_sitekey'];
+                            $secretkey = $cptch['recaptcha_secretkey'];
+                            
+                            if(!empty($sitekey) && !empty($secretkey)) {
+                                $output .=
+                                    '<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+                                    <input type="hidden" name="action" value="validate_captcha">
+                                    <input type="hidden" name="formid" value="' . $form['id'] . '">
+								    <input type="hidden" name="returnurl" value="' . $_SERVER['REQUEST_URI'] . '">
+                                    
+                                    <script src="https://www.google.com/recaptcha/api.js?render=' . $sitekey . '"></script>
+                                    
+                                    <script>
+                                        $("form#' . $structure['formid'] . '").submit(function() {
+                                            event.preventDefault();
+                                            
+                                            grecaptcha.execute("' . $sitekey . '", {
+                                                action: \'validate_captcha\'
+                                            }).then(function(token) {
+                                                $("#g-recaptcha-response").val(token);
+                                                $("form#' . $structure['formid'] . '").unbind("submit").submit();
+                                            });
+                                        });
+                                    </script>';
+                            }
+                        }
+                    }
+                    
+                    if(isset($_SESSION['message']) && isset($_SESSION['status'])) {
+                        $output .=
+                            '<div class="alert alert-' . $_SESSION['status'] . '">'
+                                . $_SESSION['message'] . 
+                            '</div>';
+                        
+                        unset($_SESSION['message']);
+                        unset($_SESSION['status']);
                     }
                     
                     $output .=
