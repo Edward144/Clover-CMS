c4bbeebfd74309116dcecd16befe5638fa1c2fdd
Ed Hall <edward.hall.144@gmail.com>
2021-08-21 17:34:12 +0100

    Sticky header on scroll

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@
 $(".navbar .dropdown-toggle").click(function() {
     $(this).parent(".nav-item").toggleClass("exp");
 });
+
+//Sticky header
+function sticky(element) {
+    var elementHeight = element.outerHeight();
+    var elementTop = element.offset()['top'];
+    
+    if($(window).scrollTop() > elementHeight + elementTop) {
+        element.addClass("sticky");
+    }
+    else if($(window).scrollTop() == 0) {
+        element.removeClass("sticky");
+    }
+    
+    if(element.attr("id") == "pageHeader" && element.siblings(".main") && element.hasClass("sticky")) {
+        element.siblings(".main").first().css({
+            "margin-top" : elementHeight
+        });
+    }
+    else {
+        element.siblings(".main").first().css({
+            "margin-top" : ""
+        });
+    }
+}
+
+$(window).scroll(function() {
+    sticky($("#pageHeader"));
+});
+
+$(document).ready(function() {
+    sticky($("#pageHeader"));
+});
