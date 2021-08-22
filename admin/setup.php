e9697ef91ffabb96b01700dd861794bf4825333c
Ed Hall <edward.hall.144@gmail.com>
2021-08-22 09:07:45 +0100

    Update setup.php

--- admin/setup.php
+++ admin/setup.php
@@ -175,6 +175,7 @@
 
                         define('ROOT_DIR', '" . $rootdir . "');
                         define('BASE_DIR', (!empty(\$_SERVER['HTTPS']) ? 'https' : 'http') . '://' . \$_SERVER['SERVER_NAME'] . ROOT_DIR);
+                        define('CMS_VERSION', 'v1.0.0');
                     ?>"
                 );
 
