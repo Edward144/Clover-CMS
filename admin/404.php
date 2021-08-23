04b6f8881e9c43ecb1f8d96972696baecca2c6d1
Ed Hall <edward.hall.144@gmail.com>
2021-08-23 21:59:50 +0100

    Setup admin 404 page

--- admin/404.php
+++ admin/404.php
@@ -1,1 +1,11 @@
-404
+<?php 
+	$title = '404 - Page Not Found';
+	require_once(dirname(__FILE__) . '/includes/header.php'); 
+?>
+
+<div class="col py-3">
+    <h1>This page does not exist</h1>
+    <p>Go to the <a href="./admin">dashboard</a> or return to the <a href="./admin" onclick="window.history.back(); return false;">last page</a> you were on</p>
+</div>
+
+<?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
