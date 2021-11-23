b6c6449bb7e612ed45210497e188b5002e9fb672
Ed Hall <edward.hall.144@gmail.com>
2021-11-23 08:59:03 +0000

    Show comment history

--- myaccount.php
+++ myaccount.php
@@ -90,67 +90,124 @@
         }
     }
 
+    //Get comments
+    $commentsCount = $mysqli->prepare("SELECT COUNT(*) FROM `comments` WHERE registered = ?");
+    $commentsCount->bind_param('i', $_SESSION['profileid']);
+    $commentsCount->execute();
+    $commentsCountResult = $commentsCount->get_result()->fetch_array()[0];
+
+    $cPagination = new pagination($commentsCountResult, 'cpage');
+    $cPagination->load();
+
+    $comments = $mysqli->prepare(
+        "SELECT comments.*, posts.name as post_name, posts.url as post_url, post_types.name as post_type FROM `comments`
+            LEFT OUTER JOIN `posts` ON posts.id = comments.post_id
+            LEFT OUTER JOIN `post_types` ON post_types.id = posts.post_type_id
+        WHERE registered = ? ORDER BY date_posted DESC LIMIT {$cPagination->itemsPerPage} OFFSET {$cPagination->offset}"
+    );
+    $comments->bind_param('i', $_SESSION['profileid']);
+    $comments->execute();
+    $commentsResult = $comments->get_result();
+
     require_once(dirname(__FILE__) . '/includes/header.php'); 
 
 ?>
 
 <div class="content">
-    <div class="my-3">
-        <div class="bg-light rounded shadow-sm p-3">
-            <span class="float-sm-end d-block text-end"><a href="<?php echo explode('?', $_SERVER['REQUEST_URI'])[0] ?>?logout" class="btn btn-dark">Sign Out</a></span>
-            <h1>My Account: <?php echo $_SESSION['profileuser']; ?></h1>
+    <div class="bg-light rounded shadow-sm my-3 p-3">
+        <span class="float-sm-end d-block text-end"><a href="<?php echo explode('?', $_SERVER['REQUEST_URI'])[0] ?>?logout" class="btn btn-dark">Sign Out</a></span>
+        <h1>My Account: <?php echo $_SESSION['profileuser']; ?></h1>
 
-            <form id="manageaccount" method="post">                
-                <div class="row">
-                    <div class="col-md form-group mb-3">
-                        <label>First Name</label>
-                        <input type="text" class="form-control" name="firstName" value="<?php echo (!empty($_POST['firstName']) ? $_POST['firstName'] : $user['first_name']); ?>" required>
-                    </div>
-
-                    <div class="col-md form-group mb-3">
-                        <label>Last Name</label>
-                        <input type="text" class="form-control" name="lastName" value="<?php echo (!empty($_POST['lastName']) ? $_POST['lastName'] : $user['last_name']); ?>" required>
-                    </div>
+        <form id="manageaccount" method="post">                
+            <div class="row">
+                <div class="col-md form-group mb-3">
+                    <label>First Name</label>
+                    <input type="text" class="form-control" name="firstName" value="<?php echo (!empty($_POST['firstName']) ? $_POST['firstName'] : $user['first_name']); ?>" required>
                 </div>
 
-                <div class="row">
-                    <div class="col-md form-group mb-3">
-                        <label>Email</label>
-                        <input type="email" class="form-control" name="email" value="<?php echo (!empty($_POST['email']) ? $_POST['email'] : $user['email']); ?>" required>
-                    </div>
+                <div class="col-md form-group mb-3">
+                    <label>Last Name</label>
+                    <input type="text" class="form-control" name="lastName" value="<?php echo (!empty($_POST['lastName']) ? $_POST['lastName'] : $user['last_name']); ?>" required>
+                </div>
+            </div>
 
-                    <div class="col-md form-group mb-3">
-                        <label>Account Type</label>
-                        <input type="text" class="form-control" value="<?php echo $user['type']; ?>" readonly>
-                    </div>
+            <div class="row">
+                <div class="col-md form-group mb-3">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name="email" value="<?php echo (!empty($_POST['email']) ? $_POST['email'] : $user['email']); ?>" required>
                 </div>
 
-                <p><a href="#" id="togglePassword" data-bs-toggle="collapse" data-bs-target="#updatePassword" aria-expanded="false">Update Password</a></p>
+                <div class="col-md form-group mb-3">
+                    <label>Account Type</label>
+                    <input type="text" class="form-control" value="<?php echo $user['type']; ?>" readonly>
+                </div>
+            </div>
 
-                <div id="updatePassword" class="row collapse">
-                    <div class="col-md form-group mb-3">
-                        <label>New Password</label>
-                        <input type="password" class="form-control" name="password" value="<?php echo (!empty($_POST['password']) ? $_POST['password'] : ''); ?>">
-                    </div>
+            <p><a href="#" id="togglePassword" data-bs-toggle="collapse" data-bs-target="#updatePassword" aria-expanded="false">Update Password</a></p>
 
-                    <div class="col-md form-group mb-3">
-                        <label>Confirm New Password</label>
-                        <input type="password" class="form-control" name="passwordConf" value="<?php echo (!empty($_POST['passwordConf']) ? $_POST['passwordConf'] : ''); ?>">
-                    </div>
+            <div id="updatePassword" class="row collapse">
+                <div class="col-md form-group mb-3">
+                    <label>New Password</label>
+                    <input type="password" class="form-control" name="password" value="<?php echo (!empty($_POST['password']) ? $_POST['password'] : ''); ?>">
                 </div>
 
-                <div class="form-group d-flex align-items-center justify-content-end">
-                    <input type="submit" class="btn btn-primary text-light" name="updateAccount" value="Save Changes">
+                <div class="col-md form-group mb-3">
+                    <label>Confirm New Password</label>
+                    <input type="password" class="form-control" name="passwordConf" value="<?php echo (!empty($_POST['passwordConf']) ? $_POST['passwordConf'] : ''); ?>">
                 </div>
+            </div>
 
-                <?php if(isset($accountmessage)) : ?>
-                    <div class="alert alert-<?php echo $status; ?> mb-0 mt-3">
-                        <?php echo $accountmessage; ?>
-                    </div>
-                <?php endif; ?>
-            </form>
-        </div>
+            <div class="form-group d-flex align-items-center justify-content-end">
+                <input type="submit" class="btn btn-primary text-light" name="updateAccount" value="Save Changes">
+            </div>
+
+            <?php if(isset($accountmessage)) : ?>
+                <div class="alert alert-<?php echo $status; ?> mb-0 mt-3">
+                    <?php echo $accountmessage; ?>
+                </div>
+            <?php endif; ?>
+        </form>
     </div>
+    
+    <?php if($commentsResult->num_rows > 0) : ?>
+        <div class="bg-light rounded shadow-sm my-3 p-3">
+            <h2>Comment history</h2>
+            
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Comment</th>
+                            <th class="text-center">Commented On</th>
+                            <th class="text-center">Date Posted</th>
+                            <th class="text-center">Approved?</th>
+                            <th class="text-center">Actions</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        <?php while($comment = $commentsResult->fetch_assoc()): ?>
+                            <tr>
+                                <td><?php echo substr($comment['content'], 0, 100) . (strlen($comment['content']) > 100 ? '...' : ''); ?></td>
+                                <td class="text-center"><a href="<?php echo $comment['post_url']; ?>"><?php echo $comment['post_name']; ?></a></td>
+                                <td class="text-center"><?php echo date('d/m/Y H:i', strtotime($comment['date_posted'])); ?></td>
+                                <td class="text-center"><?php echo ($comment['approved'] == 1 ? '<span class="fa fa-check-circle text-success"></span>' : '<span class="fa fa-times-circle text-danger"></span>'); ?></td>
+                                <td class="text-center">
+                                    <?php if($comment['approved'] == 1 && $comment['modified'] == 0) : ?>
+                                        <a href="<?php echo $comment['post_url'] . '#comment' . $comment['id']; ?>">View / Edit</a>
+                                    <?php elseif($comment['approved'] == 1 && $comment['modified'] == 1) : ?>
+                                        <a href="<?php echo $comment['post_url'] . '#comment' . $comment['id']; ?>">View</a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+            
+            <?php echo $cPagination->display(); ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
