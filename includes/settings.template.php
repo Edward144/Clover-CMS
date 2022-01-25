3a97d2a5127dbb0db9703e1533b652799e1a481b
Ed Hall <edward.hall.144@gmail.com>
2022-01-25 08:43:29 +0000

    Always allow access to my-profile

--- includes/settings.template.php
+++ includes/settings.template.php
@@ -11,7 +11,7 @@
 
     //Pages that must always be accessible by cms user roles
     //Manage content is included here as we will differentiate content by post type
-    define('ALLOWED_PAGES', ['404.php', 'index.php', 'setup.php', 'template.php', 'manage-content.php']);
+    define('ALLOWED_PAGES', ['404.php', 'index.php', 'setup.php', 'template.php', 'manage-content.php', 'my-profile.php']);
 
     //The given name for none admin users
     define('BASIC_USER', 'Subscriber');
