d30fd0dd19742f10e62daea1ec6c4d718f3e3252
Ed Hall <edward.hall.144@gmail.com>
2022-05-17 21:59:04 +0100

    Update version number

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@
         session_start();
     }
     
-    define('CMS_VERSION', 'v1.4.2');
+    define('CMS_VERSION', 'v1.4.3');
 
     $setupLoc = (!empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . '/' . explode($_SERVER['DOCUMENT_ROOT'] . '/', dirname(__DIR__))[1] . '/admin/setup';
 
