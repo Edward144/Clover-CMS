158d26ea56baa1cd53069610c7c5b3c42fddbe5f
Ed Hall <edward.hall.144@gmail.com>
2022-06-15 08:48:56 +0100

    Replace old bootstrap js links

--- admin-login/forgotten-password.php
+++ admin-login/forgotten-password.php
@@ -61,7 +61,7 @@
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-		<script src="bootstrap-5.0.1/bootstrap.min.js"></script>
+		<script src="js/bootstrap.bundle.min.js"></script>
 		<script src="https://kit.fontawesome.com/a05d626b05.js" crossorigin="anonymous"></script>
 	</head>
 	
