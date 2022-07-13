263818d69cd55f63a5f0b809dc8b594b1386d468
Ed Hall <edward.hall.144@gmail.com>
2022-07-13 09:42:57 +0100

    Remove calendar JS

--- js/main.js
+++ js/main.js
@@ -242,31 +242,3 @@ $(".comment").on("click", ".commentUpdate", function() {
         form.submit();
     }
 });
-
-//Resize calendar
-function calendar_resize(element) {
-    var width = element.outerWidth();
-
-    if(width < 576) {
-        element.addClass("calendar-sm");
-        element.removeClass("calendar-lg calendar-md");
-    }
-    else if(width < 768) {
-        element.addClass("calendar-md");
-        element.removeClass("calendar-lg calendar-sm");
-    }
-    else if(width < 992) {
-        element.addClass("calendar-lg");
-        element.removeClass("calendar-sm calendar-md");
-    }
-}
-
-$(document).ready(function() {
-    calendar_resize($(".calendar"));
-    calendar_resize($(".calendarSmall"));
-});
-
-$(window).resize(function() {
-    calendar_resize($(".calendar"));
-    calendar_resize($(".calendarSmall"));
-});
